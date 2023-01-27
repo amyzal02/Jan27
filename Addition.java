9a6d6bfaaf1252ad7e95364602c50f955ed1db86
amyzal02 <zalan@appstate.edu>
2023-01-27 18:23:04 -0500

    Subtraction base

--- Addition.java
+++ Addition.java
@@ -1,3 +1,15 @@
 public class Addition extends Operations{
     
+   public Addition(int a, int b){
+        super(a, b);
+    }
+    
+    public int operate(){
+        return a + b; 
+    }
+
+    public void print(){
+        System.out.println("You chose Addition");
+        super.print();
+    }
 }
--- Subtraction.java
+++ Subtraction.java
@@ -1,11 +1,16 @@
 public class Subtraction extends Operations {
+
+    public Subtraction(int a, int b){
+        super(a, b);
+    }
     
-    public int operate(int a, int b){
+    public int operate(){
         return a - b; 
     }
 
     public void print(){
-        System.out.println("You chose Subtraction\n");
+        System.out.println("You chose Subtraction");
+        super.print();
     }
 
 }
