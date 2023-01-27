f93f69be7dcaa94004dae11195c49532ac0f3f0e
amyzal02 <zalan@appstate.edu>
2023-01-27 18:25:13 -0500

    done?

--- Demo.java
+++ Demo.java
@@ -5,8 +5,26 @@ public class Demo{
 
         Scanner kb = new Scanner(System.in);
 
-        System.out.println("What operation do you want?\n(0) Subtraction (1) Addition");
+        System.out.print("What operation do you want?\n(0) Subtraction (1) Addition:\t");
         
         int input = kb.nextInt();
+
+        System.out.print("First Number: ");
+        int a = kb.nextInt();
+
+        System.out.print("Second Number: ");
+        int b = kb.nextInt();
+
+        Operations o; 
+
+        if(input == 0) {
+            o = new Subtraction(a, b);
+        }   else {
+            o= new Addition(a, b);
+        }
+
+        o.operate();
+
+        o.print();
     }
 }
